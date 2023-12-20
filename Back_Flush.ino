b6e1bf7dc6265030f85518ec735b90c499a2df8c
Beau Clark <beau28713@gmail.com>
2023-12-19 20:06:48 -0500

    Add files via upload

--- Back_Flush.ino
+++ Back_Flush.ino
@@ -9,12 +9,17 @@ int man_backflush_switch = 2;
 int man_control_switch = 13;
 //int tilt_switch = 4;
 
+unsigned long previous_time = 0;
+long time_interval = 1000;
+
 void setup() {
   //pinMode(tilt_switch, INPUT);
   pinMode(man_backflush_switch, INPUT);
   pinMode(man_control_switch, INPUT);
   pinMode(valve_control_relay, OUTPUT);
   pinMode(pump_control_relay, OUTPUT);
+
+  Serial.begin(9600);
 
   digitalWrite(valve_control_relay, HIGH);
   digitalWrite(pump_control_relay, HIGH);
@@ -39,20 +44,35 @@ void loop() {
 
 void valve_open() {
   digitalWrite(valve_control_relay, LOW);
-  delay(15000);
+  time_loop(15);
 }
 
 void valve_close() {
   digitalWrite(valve_control_relay, HIGH);
-  delay(15000);
+  time_loop(15);
 }
 
 void pump_on() {
   digitalWrite(pump_control_relay, LOW);
-  delay(15000);
+  time_loop(15);
 }
 
 void pump_off() {
   digitalWrite(pump_control_relay, HIGH);
-  delay(5000);
+  time_loop(5);
+}
+
+void time_loop(int timer){
+  int time_sec = 0;
+  int time_count = 0;
+  while (time_count < timer){
+    unsigned long time_started = millis();
+    if (time_started - previous_time >= time_interval){
+      previous_time = time_started;
+      time_sec = previous_time / time_interval;
+      time_count += 1;
+      Serial.println(time_count);
+      
+    }
+  }
 }
